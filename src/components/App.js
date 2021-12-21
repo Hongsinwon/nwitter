340f5ba3920637b54b4ae33d3f9c2e8dc4c81868
Honsinwon <ghdtlsdnjs12@naver.com>
2021-12-22 02:11:25 +0900

    Router & firebase setting

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import AppRouter from "components/Router";
+import { auth } from "fbase";
+
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
+  //console.log(auth.currentUser)
+  return (
+    <>
+  <AppRouter isLoggedIn={isLoggedIn} />
+  <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+  </>
+  )
+}
+
+export default App;
---
+++ src/routes/EditProfile.js
@@ -0,0 +1,5 @@
+import React from "react"; 
+
+const EditProgfile = () => <span>Edit Profile</span>;
+
+export default EditProgfile;
